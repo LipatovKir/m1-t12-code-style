8f7994011d9dd2d4fe147953b14143ab0029680f
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2022-12-29 09:01:16 +0300

    Вторая итерация Code Style

--- DepositCalculator.java
+++ DepositCalculator.java
@@ -1,41 +1,45 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent (double a, double y, int d){
-        double pay = a * Math.pow (( 1 + y / 12), 12 * d );
-        return getRounding ( pay, 2);
+    public static void main (String[] args){
+        new DepositCalculator().processUserInput();
+
+    double calculateComplexPercent(double amount, double yearRate, int yearsCount) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * yearsCount);
+        return getRounding(pay, 2);
     }
-    double calculateSimplePercent (double doubleAmount, double doubleYearRate, int depositPeriod){
-        return getRounding (doubleAmount+doubleAmount * doubleYearRate * depositPeriod, 2);
+    double calculateSimplePercent(amount, yearRate, yearsCount) {
+        return getRounding(amount + amount * yearRate * yearsCount, 2);
     }
-    double getRounding (double value, int places){
-        double ScaLe = Math.pow (10 , places);
-        return Math.round (value * ScaLe) / ScaLe;
+    double getRounding(double value, int places) {
+        double scaLe = Math.pow(10 , places);
+        return Math.round(value * scaLe) / scaLe;
     }
 
-    void doImportantJob ( ){
-        int period, action ;
+    void processUserInput() {
+        int period;
+        int depositType;
+        double percentRate = 0.06;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println ("Введите сумму вклада в рублях:") ;
+        System.out.println("Введите сумму вклада в рублях:");
         int amount = scanner.nextInt();
 
-        System.out.println ("Введите срок вклада в годах:") ;
-        period = scanner.nextInt( );
+        System.out.println("Введите срок вклада в годах:");
+        int period = scanner.nextInt();
 
-        System.out.println ("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        action = scanner.nextInt();
-        double outDoubleVar = 0;
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        depositType = scanner.nextInt();
+        double totalProfit = 0.0;
 
-        if (action == 1) {
-            outDoubleVar = calculateSimplePercent (amount, 0.06, period);
-        } else if (action == 2){
-            outDoubleVar = calculateComplexPercent (amount, 0.06, period);
+        if (depositType == 1) {
+            totalProfit = calculateSimplePercent(amount, percentRate, period);
+
+        } else if (depositType == 2) {
+            totalProfit = calculateComplexPercent(amount, percentRate, period);
         }
-        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + totalProfit);
     }
-    public static void main (String[] args){
-        new DepositCalculator()
-                .doImportantJob();
+
     }
 }
