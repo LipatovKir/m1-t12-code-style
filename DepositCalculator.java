ffbb9a72ff2c7a1bee99c8d7ae181104418eef3c
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2022-12-30 10:06:16 +0300

    3 - я итерация

--- DepositCalculator.java
+++ DepositCalculator.java
@@ -1,32 +1,37 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    public static void main (String[] args){
+
+    public static void main(String[] args) {
         new DepositCalculator().processUserInput();
+    }
 
     double calculateComplexPercent(double amount, double yearRate, int yearsCount) {
         double pay = amount * Math.pow((1 + yearRate / 12), 12 * yearsCount);
         return getRounding(pay, 2);
     }
-    double calculateSimplePercent(amount, yearRate, yearsCount) {
+
+    double calculateSimplePercent(double amount, double yearRate,int yearsCount) {
         return getRounding(amount + amount * yearRate * yearsCount, 2);
     }
+
     double getRounding(double value, int places) {
-        double scaLe = Math.pow(10 , places);
-        return Math.round(value * scaLe) / scaLe;
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
     }
 
     void processUserInput() {
         int period;
         int depositType;
         double percentRate = 0.06;
+        int amount;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
-        int amount = scanner.nextInt();
+        amount = scanner.nextInt();
 
         System.out.println("Введите срок вклада в годах:");
-        int period = scanner.nextInt();
+        period = scanner.nextInt();
 
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         depositType = scanner.nextInt();
@@ -42,4 +47,3 @@ public class DepositCalculator {
     }
 
     }
-}
